d699fea7a4dccfd438b7eb93dd0b9e266c9080b0
M Sal <salman_it24@hotmail.com>
2013-10-02 21:40:16 +0100

    Update new Sample

---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,14 @@
+C:\temp\PushNotificationSampleII\platforms\android\gen\com\pushappii\R.java \
+ : C:\temp\PushNotificationSampleII\platforms\android\res\drawable\icon.png \
+C:\temp\PushNotificationSampleII\platforms\android\res\drawable-hdpi\icon.png \
+C:\temp\PushNotificationSampleII\platforms\android\res\drawable-hdpi\ic_launcher.png \
+C:\temp\PushNotificationSampleII\platforms\android\res\drawable-ldpi\icon.png \
+C:\temp\PushNotificationSampleII\platforms\android\res\drawable-ldpi\ic_launcher.png \
+C:\temp\PushNotificationSampleII\platforms\android\res\drawable-mdpi\icon.png \
+C:\temp\PushNotificationSampleII\platforms\android\res\drawable-mdpi\ic_launcher.png \
+C:\temp\PushNotificationSampleII\platforms\android\res\drawable-xhdpi\icon.png \
+C:\temp\PushNotificationSampleII\platforms\android\res\drawable-xhdpi\ic_launcher.png \
+C:\temp\PushNotificationSampleII\platforms\android\res\layout\main.xml \
+C:\temp\PushNotificationSampleII\platforms\android\res\values\strings.xml \
+C:\temp\PushNotificationSampleII\platforms\android\res\xml\config.xml \
+C:\temp\PushNotificationSampleII\platforms\android\bin\AndroidManifest.xml \
